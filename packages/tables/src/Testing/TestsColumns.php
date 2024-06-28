27b85bb416ac26ea6bcd0b19b42b92a39a96ec40
Sandro Gehri <sandrogehri@gmail.com>
2024-06-28 14:29:55 +0200

    Add expected state to the error messages on column test assertions

--- packages/tables/src/Testing/TestsColumns.php
+++ packages/tables/src/Testing/TestsColumns.php
@@ -192,7 +192,7 @@ class TestsColumns
 
             Assert::assertTrue(
                 $state == $value,
-                message: "Failed asserting that a table column with name [{$name}] has value of [{$value}] for record [{$record->getKey()}] on the [{$livewireClass}] component.",
+                message: "Failed asserting that a table column with name [{$name}] has value of [{$value}] for record [{$record->getKey()}] on the [{$livewireClass}] component. Expected: [{$state}]",
             );
 
             return $this;
@@ -252,8 +252,8 @@ class TestsColumns
             $livewireClass = $this->instance()::class;
 
             Assert::assertTrue(
-                $column->formatState($column->getState()) == $value,
-                message: "Failed asserting that a table column with name [{$name}] has a formatted state of [{$value}] for record [{$record->getKey()}] on the [{$livewireClass}] component.",
+                ($formattedState = $column->formatState($column->getState())) == $value,
+                message: "Failed asserting that a table column with name [{$name}] has a formatted state of [{$value}] [{$column->formatState($column->getState())}] for record [{$record->getKey()}] on the [{$livewireClass}] component. Expected: [{$formattedState}]",
             );
 
             return $this;
